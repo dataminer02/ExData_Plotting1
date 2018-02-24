513b8891d9dae2a5c08e8a6f0e84a6f79f4597ca
dataminer02 <32822743+dataminer02@users.noreply.github.com>
2018-02-25 05:59:56 +0800

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,11 @@
+data_file <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?")
+data_date <- subset(data_file, Date %in% c("1/2/2007","2/2/2007"))
+data_date$Date <- as.Date(data_date$Date, format="%d/%m/%Y")
+date_time <- paste(as.Date(data_date$Date), data_date$Time)
+data_date$Datetime <- as.POSIXct(date_time)
+with(data_date, {
+  plot(Global_active_power~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+})
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,15 @@
+data_file <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?")
+data_date <- subset(data_file, Date %in% c("1/2/2007","2/2/2007"))
+data_date$Date <- as.Date(data_date$Date, format="%d/%m/%Y")
+date_time <- paste(as.Date(data_date$Date), data_date$Time)
+data_date$Datetime <- as.POSIXct(date_time)
+with(data_date, {
+  plot(Sub_metering_1~Datetime, type="l",
+       ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
